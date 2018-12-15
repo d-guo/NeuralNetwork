728bc8c805fa5cac8221853897593a39c0566f12
d-guo <dguo@ucsb.edu>
2018-12-15 02:25:11 -0800

    very improved neural network now using 5 neurons in hidden layer

--- nn.py
+++ nn.py
@@ -1,4 +1,4 @@
-# 2 layer neural network with 3 neurons in hidden layer
+# 2 layer neural network with 5 neurons in hidden layer
 #approximate f(x) = 2*relu(-x + 1) - 1
 
 import random
@@ -6,14 +6,18 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib import style
 
+#lists containing the data to be plotted for visualizing the convergence of the averageloss
 xpoints = list()
 ypoints = list()
 plt.ion()
 
+def f(x):
+	return 2 * max(0, -x + 1) - 1
+
 class neuralnet:
-	def __init__(self):	#input layer has 1 neuron, hidden layer has 3 neurons, output layer has 1 neuron
+	def __init__(self):	#input layer has 1 neuron, hidden layer has 5 neurons, output layer has 1 neuron
 		self.weights = list()	#weights will be 2d list with rows as layer and columns as neuron
-		self.biases = list()	#biases will be list with 1 bias per layer or weights
+		self.biases = list()	#biases will be list with 1 bias per layer
 		self.nodevals = list()	#3d list labels for 2d lists with rows as layer and columns as neuron
 
 		self.weights.append(list())
@@ -22,7 +26,7 @@ class neuralnet:
 		#give random values for weights and biases
 		for i in range(0, 2):
 			self.biases.append(random.uniform(0, 2))
-			for j in range(0, 3):
+			for j in range(0, 5):
 				self.weights[i].append(random.uniform(0, 2))
 
 	def averageloss(self, desiredOutputs):
@@ -61,7 +65,9 @@ class neuralnet:
 			self.nodevals[i][1].append(self.relu(self.weights[0][0] * self.nodevals[i][0][0] + self.biases[0]))
 			self.nodevals[i][1].append(self.relu(self.weights[0][1] * self.nodevals[i][0][0] + self.biases[0]))
 			self.nodevals[i][1].append(self.relu(self.weights[0][2] * self.nodevals[i][0][0] + self.biases[0]))
-			self.nodevals[i][2].append(self.weights[1][0] * self.nodevals[i][1][0] + self.weights[1][1] * self.nodevals[i][1][1] + self.weights[1][2] * self.nodevals[i][1][2] + self.biases[1])
+			self.nodevals[i][1].append(self.relu(self.weights[0][3] * self.nodevals[i][0][0] + self.biases[0]))
+			self.nodevals[i][1].append(self.relu(self.weights[0][4] * self.nodevals[i][0][0] + self.biases[0]))
+			self.nodevals[i][2].append(self.weights[1][0] * self.nodevals[i][1][0] + self.weights[1][1] * self.nodevals[i][1][1] + self.weights[1][2] * self.nodevals[i][1][2] + self.weights[1][3] * self.nodevals[i][1][3] + self.weights[1][4] * self.nodevals[i][1][4] + self.biases[1])
 
 		return self.nodevals
 
@@ -78,11 +84,15 @@ class neuralnet:
 			dzdw[i][0].append(self.weights[1][0] * self.drelu(self.weights[0][0] * self.nodevals[i][0][0] + self.biases[0]) * self.nodevals[i][0][0])
 			dzdw[i][0].append(self.weights[1][1] * self.drelu(self.weights[0][1] * self.nodevals[i][0][0] + self.biases[0]) * self.nodevals[i][0][0])
 			dzdw[i][0].append(self.weights[1][2] * self.drelu(self.weights[0][2] * self.nodevals[i][0][0] + self.biases[0]) * self.nodevals[i][0][0])
+			dzdw[i][0].append(self.weights[1][3] * self.drelu(self.weights[0][3] * self.nodevals[i][0][0] + self.biases[0]) * self.nodevals[i][0][0])
+			dzdw[i][0].append(self.weights[1][4] * self.drelu(self.weights[0][4] * self.nodevals[i][0][0] + self.biases[0]) * self.nodevals[i][0][0])
 
 			#second layer
 			dzdw[i][1].append(self.nodevals[i][1][0])
 			dzdw[i][1].append(self.nodevals[i][1][1])
 			dzdw[i][1].append(self.nodevals[i][1][2])
+			dzdw[i][1].append(self.nodevals[i][1][3])
+			dzdw[i][1].append(self.nodevals[i][1][4])
 
 		return dzdw
 
@@ -92,7 +102,7 @@ class neuralnet:
 
 		for i in range(0, numofits):
 			dzdb.append(list())
-			dzdb[i].append(self.weights[1][0] * self.drelu(self.weights[0][0] * self.nodevals[i][0][0] + self.biases[0]) + self.weights[1][1] * self.drelu(self.weights[0][1] * self.nodevals[i][0][0] + self.biases[0]) + self.weights[1][2] * self.drelu(self.weights[0][2] * self.nodevals[i][0][0] + self.biases[0]))
+			dzdb[i].append(self.weights[1][0] * self.drelu(self.weights[0][0] * self.nodevals[i][0][0] + self.biases[0]) + self.weights[1][1] * self.drelu(self.weights[0][1] * self.nodevals[i][0][0] + self.biases[0]) + self.weights[1][2] * self.drelu(self.weights[0][2] * self.nodevals[i][0][0] + self.biases[0]) + self.weights[1][3] * self.drelu(self.weights[0][3] * self.nodevals[i][0][0] + self.biases[0]) + self.weights[1][4] * self.drelu(self.weights[0][4] * self.nodevals[i][0][0] + self.biases[0]))
 			dzdb[i].append(1)
 
 		return dzdb
@@ -113,10 +123,14 @@ class neuralnet:
 			dcdw[i][0].append(dcdz * dzdw[i][0][0])
 			dcdw[i][0].append(dcdz * dzdw[i][0][1])
 			dcdw[i][0].append(dcdz * dzdw[i][0][2])
+			dcdw[i][0].append(dcdz * dzdw[i][0][3])
+			dcdw[i][0].append(dcdz * dzdw[i][0][4])
 
 			dcdw[i][1].append(dcdz * dzdw[i][1][0])
 			dcdw[i][1].append(dcdz * dzdw[i][1][1])
 			dcdw[i][1].append(dcdz * dzdw[i][1][2])
+			dcdw[i][1].append(dcdz * dzdw[i][1][3])
+			dcdw[i][1].append(dcdz * dzdw[i][1][4])
 
 		#find change in cost per change in biases
 		dzdb = self.dzdb()
@@ -133,7 +147,7 @@ class neuralnet:
 		dcdwavg.append(list())
 		dcdwavg.append(list())
 		dcdwtotal = 0
-		for k in range(0, 3):
+		for k in range(0, 5):
 			for j in range(0, 2):
 				for i in range(0, numofits):
 					dcdwtotal += dcdw[i][j][k]
@@ -151,7 +165,7 @@ class neuralnet:
 
 		#update weights
 		for i in range(0, 2):
-			for j in range(0, 3):
+			for j in range(0, 5):
 				self.weights[i][j] -= learningrate * dcdwavg[i][j]
 
 		#update biases
@@ -175,27 +189,18 @@ class neuralnet:
 			#training
 			self.train1(dataset, solutionset, learningrate)
 
-			#if training ends at maxiterations, print the final error
-			if(i == maxiterations):
-				print(self.averageloss(solutionset))
-
-			#graph to see if averageloss is converging
+			#graph to visualize the averageloss convergence
 			if(i % 100 == 0):
 				xpoints.append(i)
 				ypoints.append(self.averageloss(solutionset))
-				if(i > 300 and i < 600):
-					xpoints.pop(0)
-					ypoints.pop(0)
-				if(i > 2000):
-					xpoints.pop(0)
-					ypoints.pop(0)
 				plt.gcf().clear()
 				plt.plot(xpoints, ypoints)
 				plt.draw()
 				plt.pause(0.0000001)
 
-				#print(xpoints)
-				#print(ypoints)
+				print(xpoints)
+				print(ypoints)
+				print(self.averageloss(solutionset))
 
 			#save weights and biases if error is acceptable
 			if(self.averageloss(solutionset) < acceptableerror):
@@ -206,12 +211,42 @@ class neuralnet:
 					f.write(str(weight[1]))
 					f.write(" ")
 					f.write(str(weight[2]))
+					f.write(" ")
+					f.write(str(weight[3]))
+					f.write(" ")
+					f.write(str(weight[4]))
 					f.write("\n")
 				f.write(str(self.biases[0]))
 				f.write(" ")
 				f.write(str(self.biases[1]))
 				f.write("\n")
-				f.write(str(self.averageloss(solutionset)))
+				f.write("averageloss: " + str(self.averageloss(solutionset)))
+				f.write("\n")
+				f.write("iterations: " + str(i + 1))
+				f.close()
+				return
+
+			#if training ends at maxiterations, use the final results, even if error is not acceptable
+			if(i == maxiterations - 1):
+				f = open("trainednetworkparameters.txt", "w+")
+				for weight in self.weights:
+					f.write(str(weight[0]))
+					f.write(" ")
+					f.write(str(weight[1]))
+					f.write(" ")
+					f.write(str(weight[2]))
+					f.write(" ")
+					f.write(str(weight[3]))
+					f.write(" ")
+					f.write(str(weight[4]))
+					f.write("\n")
+				f.write(str(self.biases[0]))
+				f.write(" ")
+				f.write(str(self.biases[1]))
+				f.write("\n")
+				f.write("averageloss: " + str(self.averageloss(solutionset)))
+				f.write("\n")
+				f.write("iterations: " + str(i + 1))
 				f.close()
 				return
 
@@ -234,9 +269,9 @@ class trainednet:
 		weights2 = list()
 		biases = list()
 
-		for i in range(0, 3):
+		for i in range(0, 5):
 			weights1.append(float(l1[i]))
-		for i in range(0, 3):
+		for i in range(0, 5):
 			weights2.append(float(l2[i]))
 		for i in range(0, 2):
 			biases.append(float(l3[i]))
@@ -246,11 +281,13 @@ class trainednet:
 		self.nn.biases = biases
 
 	def f(self, x):
-		return self.nn.weights[1][0] * self.nn.relu(self.nn.weights[0][0] * x + self.nn.biases[0]) + self.nn.weights[1][1] * self.nn.relu(self.nn.weights[0][1] * x + self.nn.biases[0]) + self.nn.weights[1][2] * self.nn.relu(self.nn.weights[0][2] * x + self.nn.biases[0]) + self.nn.biases[1]
+		return self.nn.weights[1][0] * self.nn.relu(self.nn.weights[0][0] * x + self.nn.biases[0]) + self.nn.weights[1][1] * self.nn.relu(self.nn.weights[0][1] * x + self.nn.biases[0]) + self.nn.weights[1][2] * self.nn.relu(self.nn.weights[0][2] * x + self.nn.biases[0]) + self.nn.weights[1][3] * self.nn.relu(self.nn.weights[0][3] * x + self.nn.biases[0]) + self.nn.weights[1][4] * self.nn.relu(self.nn.weights[0][4] * x + self.nn.biases[0]) + self.nn.biases[1]
+
+x = 0
+tn = trainednet()
+print(tn.f(x))
+print(f(x))
 
 
-#tn = trainednet()
-#print(tn.f(-2))
-
-nn = neuralnet()
-nn.train(10e-5, 0.025, 1000000, 1000)
+#nn = neuralnet()
+#nn.train(10e-5, 0.001, 100000, 1000)
